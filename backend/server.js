a972e25f8ab1b02d221a7374a7461058f9e332f1
swarnadip sarkar <swarnadipmng@gmail.com>
2025-09-17 23:10:34 +0530

    update qr

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // my frontend origin
+    origin: ["http://localhost:5173","https://chronomark-frontend.netlify.app/"], // my frontend origin
     credentials: true, //  allow cookies & credentials
   })
 );
